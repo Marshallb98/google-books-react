8f23ce6ced3493c394f73140b3fb2658a35a7350
Marshallb98 <69322012+Marshallb98@users.noreply.github.com>
2021-03-30 01:22:57 -0500

    Added how I want the routes to be

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from './components/navbar'
 function App() {
   return (
     <Router>
-        <div className="container">
-          <h2>Google Books</h2>
-        </div>
-      </Router>
+    <div>
+    <Navbar/>
+      <Switch>
+        <Route exact path={["/", "/search"]}>
+          <Search />
+        </Route>
+        <Route exact path="/saved">
+          <Saved />
+        </Route>
+        <Route>
+          <NoMatch />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
   );
 }
 
---
+++ client/src/components/navbar.js
@@ -0,0 +1,15 @@
+import Navbar from 'react-bootstrap/Navbar';
+import React, { Component } from "react";
+import Container from 'react-bootstrap/Container'
+function App() {
+    return (
+<Container>
+  <Navbar expand="lg" variant="light" bg="light">
+    <Navbar.Brand href="#">Navbar</Navbar.Brand>
+  </Navbar>
+</Container>
+    );
+  }
+  
+  
+  export default App;
